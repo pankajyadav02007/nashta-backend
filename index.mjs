d8743445037236770d86c382f7b29c199623f572
pankaj <pankajyadav221306@gmail.com>
2026-04-16 17:52:11 +0530

    add prisma lib

--- index.mjs
+++ index.mjs
@@ -2,9 +2,27 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.status(200).json({
     name: "Hello World",
+  });
+});
+
+// login
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({
+    name: "login",
+  });
+});
+
+// signup
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({
+    name: "signup",
   });
 });
 
